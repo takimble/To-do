2833978eb6a435944f3f49cb0b9bedf1b10bce40
tatyanna <takimble1@gmail.com>
2019-10-29 15:09:36 -0400

    html

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-    
+
+
+@app.route('/todo')
+def todo():
+    return render_template('todo.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    print(request.form)
+    return 'hit endpoint'
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
